486119392a0a1855f24853807a458b7e0dbf53ba
Виктор Сокольников <vzhop@outlook.com>
2020-09-21 09:05:16 +0300

    first commit

---
+++ ClassLibrary1/Sum.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Sum
+    {
+        private string a;
+        private string b;
+
+        public Sum(string a, string b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        public string summa ()
+        {       
+            return "" + (Int32.Parse(a) + Int32.Parse(b));
+        }
+        
+        public string summa (string c)
+        {       
+            return "" + (Int32.Parse(a) + Int32.Parse(b)+ Int32.Parse(c));
+        }
+
+    }
+}
